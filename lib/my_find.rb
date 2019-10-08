1359fea00de8671041efa5e1d92a481ad2df132b
Ibrahim  Akar <ibsakar@gmail.com>
2019-10-08 15:31:33 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,9 +4,11 @@ def my_find(collection)
   i = 0 
   while i < collection.length 
     return 
-      collection[i] if 
+      collection[i] 
+      return 
+      if 
       yield(collection[i])
-    end
+    
     i = i + 1
   end
 end
